146daf01e1ccce3a35f3568acba49f1f4f9b0874
Ajzahtiri <stickboi@live.co.uk>
2013-12-22 07:55:05 +0000

    menu framework works

---
+++ Snowy Castle/Snowy Castle/ScreenManager/Background.cs
@@ -0,0 +1,52 @@
+﻿using System;
+using Microsoft.Xna.Framework;
+using Microsoft.Xna.Framework.Content;
+using Microsoft.Xna.Framework.Graphics;
+
+namespace Snowy_Castle
+{
+
+    class BackgroundScreen : GameScreen
+    {
+        ContentManager content;
+        Texture2D background;
+
+        public BackgroundScreen()
+        {
+            TransitionOnTime = TimeSpan.FromSeconds(0.5);
+            TransitionOffTime = TimeSpan.FromSeconds(0.5);
+        }
+
+        public override void LoadContent()
+        {
+            if (content == null)
+            {
+                content = new ContentManager(ScreenManager.Game.Services, "Content");
+            }
+            background = content.Load<Texture2D>("Textures\\Winter_Castle");
+        }
+
+        public override void UnloadContent()
+        {
+            content.Unload();
+        }
+
+        public override void Update(GameTime gameTime, bool otherScreenHasFocus, bool coveredByOtherScreen)
+        {
+            base.Update(gameTime, otherScreenHasFocus, false);
+        }
+
+        public override void Draw(GameTime gameTime)
+        {
+            SpriteBatch spriteBatch = ScreenManager.SpriteBatch;
+            Viewport viewport = ScreenManager.GraphicsDevice.Viewport;
+            Rectangle fullscreen = new Rectangle(0, 0, viewport.Width, viewport.Height);
+
+            spriteBatch.Begin();
+
+            spriteBatch.Draw(background, fullscreen, new Color(TransitionAlpha, TransitionAlpha, TransitionAlpha));
+
+            spriteBatch.End();
+        }
+    }
+}
---
+++ Snowy Castle/Snowy Castle/ScreenManager/PlayerIndexEventArgs.cs
@@ -0,0 +1,22 @@
+﻿using System;
+using Microsoft.Xna.Framework;
+
+namespace Snowy_Castle
+{
+    class PlayerIndexEventArgs : EventArgs
+    {
+        PlayerIndex playerIndex;
+        public PlayerIndexEventArgs(PlayerIndex pi)
+        {
+            this.playerIndex = pi;
+        }
+
+        public PlayerIndex PlayerIndex
+        {
+            get 
+            { 
+                return playerIndex;
+            }
+        }        
+    }
+}
